6e146331a0ac86ac26192c1329132df94231a9eb
GoldiCleo <goldi@mybusinessdna.com.au>
2024-06-12 10:26:43 +1000

    fe and be

---
+++ api/routes/hotels.js
@@ -0,0 +1,28 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//Create
+router.post("/", verifyAdmin, createHotel);
+
+//Update
+router.put("/:id", verifyAdmin, updateHotel);
+
+//delete
+router.delete("/:id", verifyAdmin, deleteHotel);
+
+//Get
+router.get("/find/:id", getHotel);
+
+//Get All
+router.get("/", getHotels);
+
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+
+
+export default router;
